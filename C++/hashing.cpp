8de75812342da9e102d21360cb167a6fb233a90b
Mahdy Berriri <44408019+Maa-ddy@users.noreply.github.com>
2024-09-02 01:22:28 +0100

    fix logic of prefix suffix hash calculation different mod values

--- C++/hashing.cpp
+++ C++/hashing.cpp
@@ -1,16 +1,37 @@
-// prefix, suffix hashing
 
-string s; cin >> s;
-  ll n = s.length();
-  ll pref_hash[n]; pref_hash[0] = s[0] - 'a' + 1;
-  ll suf_hash[n]; suf_hash[n - 1] = s[n - 1] - 'a' + 1;
-  ll pow = prime;
-  for (int left = 1, right = n - 2; left < n; left++, right--) {
-    pref_hash[left] = (pref_hash[left - 1] + s[left] - 'a' + 1) * pow;
+ull prime = 31;
+ull mod = 1000004123;
+
+//ull mod = 1000000009; // these are the usual used values, but sometimes they construct tests to make hashing with this mod fail
+//ull mod = 1000000007; // better use the first mod, or other values > billion. check this url: http://compoasso.free.fr/primelistweb/page/prime/liste_online_en.php
+
+void largest_prefix_equal_suffix() {
+  string s; cin >> s;
+  ull n = s.length();
+  ull pref_hash[n]; pref_hash[0] = s[0] - 'a' + 1;
+  ull pow = prime;
+  ull pref_pow[n]; pref_pow[0] = 1;
+  for (int left = 1; left < n; left++) {
+    pref_hash[left] = pref_hash[left - 1] + ((s[left] - 'a' + 1) * pow) % mod;
     pref_hash[left] %= mod;
 
-    suf_hash[right] = (suf_hash[right + 1] + s[right] - 'a' + 1 ) * pow;
-    suf_hash[right] %= mod;
-    
+    pref_pow[left] = pow;
     pow = (pow * prime) % mod;
   }
+
+  ull ans = -1;
+  for (int k = 0; k < n; k++) {
+    if (n - 1 - k < 0) break;
+    if (n - 1 - k < 1) break;
+    ull prev = (n - 1 - k - 1 < 0) ? 0 : pref_hash[n - 1 - k - 1];
+    ull suf_hash = (((pref_hash[n - 1] - prev + mod) % mod) * modInverse(pref_pow[n - 1 - k], mod)) % mod;
+    if (pref_hash[k] == suf_hash) ans = k;
+  }
+
+  if (ans == -1) {
+    cout << "NO\n";
+  } else {
+    cout << "YES\n" << s.substr(0, ans + 1) << endl;
+ }
+
+}
